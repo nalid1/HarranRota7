c1bf817f8ae7790ffd55fef98e2aa5f516312c33
Dilan Tunc <dilantunc11@hotmail.com>
2022-12-24 21:17:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/harranrota/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.harranrota;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton
+            kutuphane,
+            yemekhane,
+            tip_fakultesi,
+            muhendislik,
+            rektorluk,
+            hesap_makinesi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        kutuphane = findViewById(R.id.kutuphane);
+        yemekhane = findViewById(R.id.yemekhane);
+        tip_fakultesi = findViewById(R.id.tip_fakultesi);
+        muhendislik = findViewById(R.id.muhendislik);
+        rektorluk = findViewById(R.id.rektorluk);
+        hesap_makinesi = findViewById(R.id.hesap_makinesi);
+
+
+
+
+        kutuphane.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this, "kütüphaneye rota oluşturuluyor", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,kutuphaneActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        yemekhane.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //    Toast.makeText(MainActivity.this, "yemekhaneye rota oluşturuluyor", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,yemekhaneActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        tip_fakultesi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(MainActivity.this, "Tıp fakültesine rota oluşturuluyor", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,tipActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        muhendislik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //   Toast.makeText(MainActivity.this, "mühendislik fakültesine rota oluşturuluyor", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,muhendislikActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        rektorluk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(MainActivity.this, "Rektörlük binasına rota oluşturuluyor", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,rektorlukActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        hesap_makinesi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hesap Makinesi Başlatılıyor", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/harranrota/muhendislikActivity.java
@@ -0,0 +1,42 @@
+package com.example.harranrota;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class muhendislikActivity  extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muhendislik);
+
+        SupportMapFragment mapFragment2 = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.muhendislikMap);
+        mapFragment2.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap1= googleMap;
+        LatLng muhendislik = new LatLng(37.1724, 39.0034);
+        mMap1.moveCamera(CameraUpdateFactory.newLatLngZoom(muhendislik, 15));
+        mMap1.addMarker(new MarkerOptions().position(muhendislik).title("Mühendislik Fakültesi"));
+
+
+
+    }
+};
